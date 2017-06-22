3bddd7401f2b87a07d8242002c077dd9b43788c8
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-06-22 16:13:59 +0100

    register node

--- client/client.go
+++ client/client.go
@@ -1,8 +1,10 @@
 package client
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/storageos/discovery/types"
@@ -17,11 +19,13 @@ type Client interface {
 	ClusterRegisterNode(clusterID, nodeID, name, advertiseIP string) (*types.Cluster, error)
 }
 
+// DefaultClient - default discovery client
 type DefaultClient struct {
 	endpoint string
 	client   *http.Client
 }
 
+// New - create new discovery client
 func New(options ...Option) *DefaultClient {
 	client := DefaultClient{
 		endpoint: DefaultEndpoint,
@@ -35,8 +39,9 @@ func New(options ...Option) *DefaultClient {
 	return &client
 }
 
+// ClusterGet - get specified cluster by ID
 func (c *DefaultClient) ClusterGet(ref string) (*types.Cluster, error) {
-	req, err := http.NewRequest("GET", c.endpoint+"/cluster/"+ref, nil)
+	req, err := http.NewRequest("GET", c.endpoint+"/clusters/"+ref, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -46,7 +51,7 @@ func (c *DefaultClient) ClusterGet(ref string) (*types.Cluster, error) {
 		return nil, err
 	}
 
-	if resp.StatusCode != 201 {
+	if resp.StatusCode != http.StatusCreated {
 		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 
@@ -58,6 +63,41 @@ func (c *DefaultClient) ClusterGet(ref string) (*types.Cluster, error) {
 	return &cluster, nil
 }
 
+// ClusterRegisterNode - register node to cluster
+func (c *DefaultClient) ClusterRegisterNode(clusterID, nodeID, name, advertiseIP string) (*types.Cluster, error) {
+	node := types.Node{
+		ID:          nodeID,
+		Name:        name,
+		AdvertiseIP: advertiseIP,
+	}
+	reqBody, err := json.Marshal(&node)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", c.endpoint+"/clusters/"+clusterID, ioutil.NopCloser(bytes.NewBuffer(reqBody)))
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	var cluster types.Cluster
+	if err := json.NewDecoder(resp.Body).Decode(&cluster); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal response from discovery service: %s", err)
+	}
+
+	return &cluster, nil
+}
+
+// WithEndpoint - override default endpoint
 func WithEndpoint(endpoint string) Option {
 	return OptionFn(func(c *DefaultClient) error {
 		c.endpoint = endpoint
