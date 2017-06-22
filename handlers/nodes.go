a10af60d00641ad84cb2411dd5c76302a439fe84
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-06-22 15:19:59 +0100

    node management

---
+++ handlers/nodes.go
@@ -0,0 +1,82 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/storageos/discovery/handlers/httperror"
+	"github.com/storageos/discovery/types"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/storageos/discovery/pkg/lockstring"
+)
+
+func init() {
+	tokenCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "endpoint_token_requests_total",
+			Help: "How many /token requests processed, partitioned by status code and HTTP method.",
+		},
+		[]string{"code", "method"},
+	)
+	prometheus.MustRegister(tokenCounter)
+}
+
+var (
+	currentLeader lockstring.LockString
+	tokenCounter  *prometheus.CounterVec
+)
+
+func (s *Server) clusterHandler(w http.ResponseWriter, r *http.Request) {
+
+	cluster, err := s.clusterManager.Get(getParam(paramCluster, r))
+	if err != nil {
+		httperror.Error(w, r, err.Error(), http.StatusInternalServerError, newCounter)
+	}
+
+	bts, err := json.Marshal(cluster)
+	if err != nil {
+		httperror.Error(w, r, err.Error(), http.StatusInternalServerError, newCounter)
+		return
+	}
+
+	w.Write(bts)
+	tokenCounter.WithLabelValues(strconv.Itoa(http.StatusOK), r.Method).Add(1)
+}
+
+func (s *Server) registerNodeHandler(w http.ResponseWriter, r *http.Request) {
+	clusterID := getParam(paramCluster, r)
+
+	var node types.Node
+	if err := json.NewDecoder(r.Body).Decode(node); err != nil {
+		httperror.Error(w, r, err.Error(), http.StatusInternalServerError, newCounter)
+		return
+	}
+
+	updated, err := s.clusterManager.RegisterNode(clusterID, &node)
+	if err != nil {
+		httperror.Error(w, r, err.Error(), http.StatusInternalServerError, newCounter)
+		return
+	}
+
+	bts, err := json.Marshal(updated)
+	if err != nil {
+		httperror.Error(w, r, err.Error(), http.StatusInternalServerError, newCounter)
+		return
+	}
+
+	w.Write(bts)
+	newCounter.WithLabelValues("200", r.Method).Add(1)
+
+}
+
+func (s *Server) deleteClusterHandler(w http.ResponseWriter, r *http.Request) {
+	err := s.clusterManager.Delete(getParam(paramCluster, r))
+	if err != nil {
+		httperror.Error(w, r, err.Error(), http.StatusInternalServerError, newCounter)
+		return
+	}
+
+	newCounter.WithLabelValues("200", r.Method).Add(1)
+}
