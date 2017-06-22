4cfb80fcf5f130a608a95ebd54408ecea55c86a4
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-06-22 14:50:00 +0100

    rewritten cluster create

--- handlers/new.go
+++ handlers/new.go
@@ -1,26 +1,21 @@
 package handlers
 
 import (
-	"bytes"
-	"crypto/rand"
-	"encoding/hex"
-	"errors"
-	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"path"
 	"strconv"
-	"time"
 
-	"github.com/storageos/discovery/handlers/httperror"
-	"github.com/coreos/etcd/client"
+	"encoding/json"
+
+	"github.com/storageos/discovery/types"
+
 	"github.com/prometheus/client_golang/prometheus"
-	"golang.org/x/net/context"
+	"github.com/storageos/discovery/handlers/httperror"
 )
 
 var newCounter *prometheus.CounterVec
-var cfg *client.Config
+
+// var cfg *client.Config
 var discHost string
 
 func init() {
@@ -34,86 +29,31 @@ func init() {
 	prometheus.MustRegister(newCounter)
 }
 
-func generateCluster() string {
-	b := make([]byte, 16)
-	_, err := rand.Read(b)
-	if err != nil {
-		return ""
-	}
-
-	return hex.EncodeToString(b)
-}
-
-func Setup(etcdHost, disc string) {
-	cfg = &client.Config{
-		Endpoints: []string{etcdHost},
-		Transport: client.DefaultTransport,
-		// set timeout per request to fail fast when the target endpoint is unavailable
-		HeaderTimeoutPerRequest: time.Second,
-	}
-
-	u, _ := url.Parse(etcdHost)
-	currentLeader.Set(u.Host)
-	discHost = disc
-}
-
-func setupToken(size int) (string, error) {
-	token := generateCluster()
-	if token == "" {
-		return "", errors.New("Couldn't generate a token")
-	}
-
-	c, _ := client.New(*cfg)
-	kapi := client.NewKeysAPI(c)
-
-	key := path.Join("_etcd", "registry", token)
-
-	resp, err := kapi.Create(context.Background(), path.Join(key, "_config", "size"), strconv.Itoa(size))
-	if err != nil {
-		return "", errors.New(fmt.Sprintf("Couldn't setup state %v %v", resp, err))
-	}
-
-	return token, nil
-}
-
-func deleteToken(token string) error {
-	c, _ := client.New(*cfg)
-	kapi := client.NewKeysAPI(c)
-
-	if token == "" {
-		return errors.New("No token given")
-	}
-
-	_, err := kapi.Delete(
-		context.Background(),
-		path.Join("_etcd", "registry", token),
-		&client.DeleteOptions{Recursive: true},
-	)
-
-	return err
-}
-
-func NewTokenHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) newClusterHandler(w http.ResponseWriter, r *http.Request) {
 	var err error
 	size := 3
-	s := r.FormValue("size")
-	if s != "" {
-		size, err = strconv.Atoi(s)
+	sz := r.FormValue("size")
+	if sz != "" {
+		size, err = strconv.Atoi(sz)
 		if err != nil {
 			httperror.Error(w, r, err.Error(), http.StatusBadRequest, newCounter)
 			return
 		}
 	}
-	token, err := setupToken(size)
-
+	cluster, err := s.clusterManager.Create(types.ClusterCreateOps{Size: size})
 	if err != nil {
-		log.Printf("setupToken returned: %v", err)
-		httperror.Error(w, r, "Unable to generate token", 400, newCounter)
+		httperror.Error(w, r, err.Error(), http.StatusInternalServerError, newCounter)
 		return
 	}
 
-	log.Println("New cluster created", token)
+	log.Println("New cluster created", cluster.ID)
 
-	fmt.Fprintf(w, "%s/%s", bytes.TrimRight([]byte(discHost), "/"), token)
+	bts, err := json.Marshal(cluster)
+	if err != nil {
+		httperror.Error(w, r, err.Error(), http.StatusInternalServerError, newCounter)
+		return
+	}
+
+	w.Write(bts)
 	newCounter.WithLabelValues("200", r.Method).Add(1)
 }
