c6ec6455a81ea877c967509e310b178ecaf67bfc
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-06-23 16:51:18 +0100

    port config

--- discovery.go
+++ discovery.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/storageos/discovery/cluster"
 	"github.com/storageos/discovery/handlers"
@@ -9,7 +11,21 @@ import (
 	"github.com/storageos/discovery/util/codecs"
 )
 
+// DefaultPort - default port to run
+const DefaultPort = 8081
+
+// EnvPort - port to run application on
+const EnvPort = "PORT"
+
 func main() {
+	port := DefaultPort
+	if os.Getenv(EnvPort) != "" {
+		p, err := strconv.Atoi(os.Getenv(EnvPort))
+		if err != nil {
+			log.Fatalf("invalid port: %s", err)
+		}
+		port = p
+	}
 
 	db, err := boltdb.New("discovery.db")
 	if err != nil {
@@ -18,6 +34,6 @@ func main() {
 
 	clusterManager := cluster.New(db, codecs.DefaultSerializer())
 
-	srv := handlers.NewServer(8081, clusterManager)
+	srv := handlers.NewServer(port, clusterManager)
 	log.Fatal(srv.Start())
 }
